efc8a5d84205a35bcb5e6c0b5537c22c34a23db4
Magna011 <129706360+Magna011@users.noreply.github.com>
2023-08-20 00:58:06 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-react-vite-mumdsa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-react-vite-mumdsa)
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+import Modul from './components/Modul/Modul';
+
+function App() {
+  return (
+    <>
+    <Modul/>
+    </>
+  );
+}
+
+export default App;
